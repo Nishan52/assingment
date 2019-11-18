8c4386bc93f9c61a0519b9bbf9d34fc90b923489
Nishan52 <herculeskhatri@gmail.com>
2019-11-18 21:20:54 +0545

    to calculaate power using function

---
+++ practice2.py
@@ -0,0 +1,7 @@
+#to calculate power using the function
+def power(a,b):
+    return a**b
+num1=float(input("Enter the base value "))
+num2=float(input("Enter the power given to the base value "))
+result=power(num1,num2)
+print("The result of the given number is ",result)
